5a378a2a482fecdfcc9f3d5b3591a2fd0c462094
Gil Mandelbaum <gilmandelbaum@Gils-MacBook-Pro.local>
2020-03-02 00:16:06 -0500

    data set notebooks working

--- data_set_analysis_helper_functions.py
+++ data_set_analysis_helper_functions.py
@@ -110,11 +110,14 @@ def stimSuccessRate (dataSet,Mouse_Date_FileName):
         cellType = sessionInfo["D"]
         sideOfStim = sessionInfo["SideOfStim"]        
         dataSetPerSession = dataSet[i]
+        
     
+        
         if cellType==1:
             
             if sideOfStim =="R":
-                licksStim = len(dataSetPerSession["L"]["ipsi"]["12"].dropna())
+                
+                licksStim= len(dataSetPerSession["L"]["ipsi"]["12"].dropna())
                 licksOtherSide = len(dataSetPerSession["R"]["ipsi"]["11"].dropna())
                 licksStimControl = len(dataSetPerSession["R"]["ipsi"]["12"].dropna())
                 licksOtherSideControl = len(dataSetPerSession["L"]["ipsi"]["11"].dropna())
@@ -162,7 +165,7 @@ def extract_data_set_respose_time (Mouse_Date_FileName,rt_stay_right,rt_switch_r
                                         "NOswitchSide_Stay"])
     
     
-    leng = len (rt_stay_right)
+    leng = len (rt_stay_right) #could look at any of the data set lists. 
     
     for i in range (leng):
         
@@ -341,4 +344,102 @@ def stimLickDirections (dataSet,Mouse_Date_FileName):
         
        
     return (dfResults)
+
+
+
+
+def extract_data_set_consumption_times (Mouse_Date_FileName,
+                                        Reward_NoReward_stay_right,
+                                        Reward_NoReward_switch_right,
+                                        Reward_NoReward_stay_left,
+                                        Reward_NoReward_switch_left): 
     
+    
+    data_list = list()
+
+    leng=len(Reward_NoReward_stay_right)
+    
+        
+    for i in range (leng):
+        
+        dfResults = pd.DataFrame(columns = ["switchSide_switch","switchSide_Stay",
+                                        "NOswitchSide_switch","NOswitchSide_Stay"])
+        
+        sessionInfo = Mouse_Date_FileName.iloc[i,:]
+        
+        cellType = sessionInfo["D"]
+        sideOfStim = sessionInfo["SideOfStim"]        
+       
+        Reward_NoReward_stay_right_i = Reward_NoReward_stay_right[i].median(axis=1)
+        Reward_NoReward_switch_right_i = Reward_NoReward_switch_right[i].median(axis=1)
+        Reward_NoReward_stay_left_i = Reward_NoReward_stay_left[i].median(axis=1)
+        Reward_NoReward_switch_left_i = Reward_NoReward_switch_left[i].median(axis=1)
+    
+        if cellType==1:
+            
+            if sideOfStim =="R":
+                
+                switchSide_switch = Reward_NoReward_switch_left_i
+                switchSide_Stay = Reward_NoReward_stay_right_i
+                
+                NOswitchSide_switch = Reward_NoReward_switch_right_i
+                NOswitchSide_Stay = Reward_NoReward_stay_left_i
+                
+                
+      
+            elif sideOfStim =="L":
+                
+                switchSide_switch = Reward_NoReward_switch_right_i
+                switchSide_Stay = Reward_NoReward_stay_left_i
+                
+                NOswitchSide_switch = Reward_NoReward_switch_left_i
+                NOswitchSide_Stay = Reward_NoReward_stay_right_i
+                
+                
+            
+        if cellType==2:
+            
+            if sideOfStim =="R":
+                
+                switchSide_switch = Reward_NoReward_switch_right_i
+                switchSide_Stay = Reward_NoReward_stay_left_i
+                
+                NOswitchSide_switch = Reward_NoReward_switch_left_i
+                NOswitchSide_Stay = Reward_NoReward_stay_right_i
+                
+            elif sideOfStim =="L":
+            
+                switchSide_switch = Reward_NoReward_switch_left_i
+                switchSide_Stay = Reward_NoReward_stay_right_i
+                
+                NOswitchSide_switch = Reward_NoReward_switch_right_i
+                NOswitchSide_Stay = Reward_NoReward_stay_left_i
+                
+        
+        dfResults = pd.DataFrame({'switchSide_switch': switchSide_switch,
+                                  "switchSide_Stay": switchSide_Stay, 
+                                  "NOswitchSide_switch": NOswitchSide_switch, 
+                                  "NOswitchSide_Stay": NOswitchSide_Stay})
+        
+        
+        data_list.append(dfResults)
+       
+    return (data_list)
+
+
+
+
+def generating_data_set_consumption_times (Mouse_Date_FileName,df_list,condition):
+    leng = len(Mouse_Date_FileName)
+    df = pd.DataFrame()
+    for i in range (leng):
+        sessionInfo = Mouse_Date_FileName.iloc[i,:]
+        mouse = sessionInfo["Mouse"] 
+        date = sessionInfo["Date"]
+    
+        name = str(mouse)+"_"+str(date)
+        
+        session = pd.DataFrame(df_list[i][condition])
+        session = session.rename(columns={condition: name})
+        df = pd.concat([df,session],axis=1, sort=False)
+    return (df)
