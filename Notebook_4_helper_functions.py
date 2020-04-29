0b45cb10bada684e31967495f83acfe8492a29f4
Gil Mandelbaum <gilmandelbaum@Gils-MacBook-Pro.local>
2020-04-29 19:13:44 -0400

    change name in function for clarity

--- Notebook_4_helper_functions.py
+++ Notebook_4_helper_functions.py
@@ -11,14 +11,14 @@ import pandas as pd
 
 #fuction that adds a flag 
 
-def tag_period(beh_photo_align, period_name, states_list):
+def tag_period(behavior_data_lick, period_name, states_list):
     
     Period_rows_list = []
 
     for states in states_list:
         state_start = states[0]
         state_end = states[1]
-        Period_rows_list.append(beh_photo_align.loc[(beh_photo_align['sTrial_start'] == state_start) & (beh_photo_align['sTrial_end'] == state_end)])
+        Period_rows_list.append(behavior_data_lick.loc[(behavior_data_lick['sTrial_start'] == state_start) & (behavior_data_lick['sTrial_end'] == state_end)])
         
     Period_rows = pd.concat(Period_rows_list)
     Period_rows = Period_rows.sort_index()
