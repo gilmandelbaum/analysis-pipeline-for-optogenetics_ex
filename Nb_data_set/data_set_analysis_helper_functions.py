d3885cb0b729853302a27a9bfa1b81c00c98dd14
Gil Mandelbaum <gilmandelbaum@Gils-MBP.cable.rcn.com>
2020-05-25 19:45:25 -0400

    addition of data_set_direction_consumption_licks notebook

--- Nb_data_set/data_set_analysis_helper_functions.py
+++ Nb_data_set/data_set_analysis_helper_functions.py
@@ -21,7 +21,7 @@ def loadSession_pickle (Mouse_Date_FileName,data_dir_output,HowManyBack,folder_n
         __mouse = "__"+mouse
         data_day= '20'+str(date)[:2]+'_'+str(date)[2:4]+'_'+str(date)[4:6]
         
-        root = Path(data_dir_output+"/"+mouse+"/"+data_day+__mouse+'/'+str(HowManyBack)+"_Back")
+        root = Path(data_dir_output+"/"+mouse+"/"+data_day+__mouse+'/'+str(HowManyBack)+"_Back"+"/"+"analysis")
         #print (mouse+"/"+data_day)
         d = folder_name+"_"+seq_str+"/"+file_name+'.pickle'
         
@@ -33,6 +33,7 @@ def loadSession_pickle (Mouse_Date_FileName,data_dir_output,HowManyBack,folder_n
         
         
         data = pickle.load(fileToOpen)
+        #data_final= data.drop("Unnamed: 0",axis=1)
         data_list.append(data)
         
     return (data_list)
@@ -55,12 +56,13 @@ def loadSession_csv (Mouse_Date_FileName,data_dir_output,HowManyBack,folder_name
         __mouse = "__"+mouse
         data_day= '20'+str(date)[:2]+'_'+str(date)[2:4]+'_'+str(date)[4:6]
         
-        root = data_dir_output+"/"+mouse+"/"+data_day+__mouse+'/'+str(HowManyBack)+"_Back/"
+        root = data_dir_output+"/"+mouse+"/"+data_day+__mouse+'/'+str(HowManyBack)+"_Back"+"/"+"analysis"+"/"
         #print (mouse+"/"+data_day)
         d = folder_name+"_"+seq_str+"/"+file_name+'.csv'
         
         data = pd.read_csv(root+d)
-        data_list.append(data)
+        data_final= data.drop("Unnamed: 0",axis=1)
+        data_list.append(data_final)
         
     return (data_list)
 
@@ -345,8 +347,9 @@ def stimLickDirections (dataSet,Mouse_Date_FileName):
        
     return (dfResults)
 
-
-
+"""
+used in data_set_time_consumption_licks
+"""
 
 def extract_data_set_consumption_times (Mouse_Date_FileName,
                                         Reward_NoReward_stay_right,
@@ -443,3 +446,283 @@ def generating_data_set_consumption_times (Mouse_Date_FileName,df_list,condition
         session = session.rename(columns={condition: name})
         df = pd.concat([df,session],axis=1, sort=False)
     return (df)
+
+
+
+
+"""
+used in data_set_direction_consumption_licks
+"""
+
+
+def extract_data_set_consumption_times_std (Mouse_Date_FileName,
+                                        Reward_NoReward_stay_right,
+                                        Reward_NoReward_switch_right,
+                                        Reward_NoReward_stay_left,
+                                        Reward_NoReward_switch_left): 
+    
+    
+    data_list = list()
+
+    leng=len(Reward_NoReward_stay_right)
+    
+        
+    for i in range (leng):
+        
+        dfResults = pd.DataFrame(columns = ["switchSide_switch","switchSide_Stay",
+                                        "NOswitchSide_switch","NOswitchSide_Stay"])
+        
+        sessionInfo = Mouse_Date_FileName.iloc[i,:]
+        
+        cellType = sessionInfo["D"]
+        sideOfStim = sessionInfo["SideOfStim"]        
+       
+        Reward_NoReward_stay_right_i_std = Reward_NoReward_stay_right[i].std(axis=1)
+        Reward_NoReward_switch_right_i_std = Reward_NoReward_switch_right[i].std(axis=1)
+        Reward_NoReward_stay_left_i_std = Reward_NoReward_stay_left[i].std(axis=1)
+        Reward_NoReward_switch_left_i_std = Reward_NoReward_switch_left[i].std(axis=1)
+        
+    
+        if cellType==1:
+            
+            if sideOfStim =="R":
+                
+                
+                switchSide_switch_std = Reward_NoReward_switch_left_i_std
+                switchSide_Stay_std = Reward_NoReward_stay_right_i_std
+                NOswitchSide_switch_std = Reward_NoReward_switch_right_i_std
+                NOswitchSide_Stay_std = Reward_NoReward_stay_left_i_std
+                
+                
+      
+            elif sideOfStim =="L":
+                
+                switchSide_switch_std = Reward_NoReward_switch_right_i_std
+                switchSide_Stay_std = Reward_NoReward_stay_left_i_std
+                NOswitchSide_switch_std = Reward_NoReward_switch_left_i_std
+                NOswitchSide_Stay_std = Reward_NoReward_stay_right_i_std
+                
+                
+            
+        if cellType==2:
+            
+            if sideOfStim =="R":
+                
+                switchSide_switch_std = Reward_NoReward_switch_right_i_std
+                switchSide_Stay_std = Reward_NoReward_stay_left_i_std
+                NOswitchSide_switch_std = Reward_NoReward_switch_left_i_std
+                NOswitchSide_Stay_std = Reward_NoReward_stay_right_i_std
+                
+            elif sideOfStim =="L":
+            
+                switchSide_switch_std = Reward_NoReward_switch_left_i_std
+                switchSide_Stay_std = Reward_NoReward_stay_right_i_std
+                NOswitchSide_switch_std = Reward_NoReward_switch_right_i_std
+                NOswitchSide_Stay_std = Reward_NoReward_stay_left_i_std
+                
+        
+        dfResults = pd.DataFrame({"switchSide_switch_std":switchSide_switch_std,
+                                  "switchSide_Stay_std": switchSide_Stay_std,
+                                  "NOswitchSide_switch_std":NOswitchSide_switch_std,
+                                  "NOswitchSide_Stay_std":NOswitchSide_Stay_std})
+        
+        
+        data_list.append(dfResults)
+       
+    return (data_list)
+
+
+def generating_data_set_consumption_times_2 (Mouse_Date_FileName,df_list,condition,tag=''):
+    leng = len(Mouse_Date_FileName)
+    df = pd.DataFrame()
+    for i in range (leng):
+        sessionInfo = Mouse_Date_FileName.iloc[i,:]
+        mouse = sessionInfo["Mouse"] 
+        date = sessionInfo["Date"]
+    
+        name = str(mouse)+"_"+str(date)+tag
+        
+        session = pd.DataFrame(df_list[i][condition])
+        session = session.rename(columns={condition: name})
+        df = pd.concat([df,session],axis=1, sort=False)
+    return (df)
+
+
+
+def extract_data_set_consumption_directions (Mouse_Date_FileName,
+                                        Reward_NoReward_stay_right_directions,
+                                        Reward_NoReward_switch_right_directions,
+                                        Reward_NoReward_stay_left_directions,
+                                        Reward_NoReward_switch_left_directions): 
+    
+    
+    data_list = list()
+
+    leng=len(Reward_NoReward_stay_right_directions)
+    
+    
+        
+    for i in range (leng):
+        
+        dfResults = pd.DataFrame(columns = ["switchSide_switch","switchSide_Stay",
+                                        "NOswitchSide_switch","NOswitchSide_Stay"])
+        
+        sessionInfo = Mouse_Date_FileName.iloc[i,:]
+        
+        cellType = sessionInfo["D"]
+        sideOfStim = sessionInfo["SideOfStim"]        
+       
+        
+
+        Reward_NoReward_stay_right_i_columns = list(Reward_NoReward_stay_right_directions[i].columns.values)
+        Reward_NoReward_stay_right_i_r = (Reward_NoReward_stay_right_directions[i][Reward_NoReward_stay_right_i_columns]==2).sum(axis=1)
+        Reward_NoReward_stay_right_i_l = (Reward_NoReward_stay_right_directions[i][Reward_NoReward_stay_right_i_columns]==1).sum(axis=1)
+        
+        
+
+        Reward_NoReward_switch_right_i_columns = list(Reward_NoReward_switch_right_directions[i].columns.values)
+        Reward_NoReward_switch_right_i_r = (Reward_NoReward_switch_right_directions[i][Reward_NoReward_switch_right_i_columns]==2).sum(axis=1)
+        Reward_NoReward_switch_right_i_l = (Reward_NoReward_switch_right_directions[i][Reward_NoReward_switch_right_i_columns]==1).sum(axis=1)
+        
+        
+        
+        Reward_NoReward_stay_left_i_columns = list(Reward_NoReward_stay_left_directions[i].columns.values)
+        Reward_NoReward_stay_left_i_r = (Reward_NoReward_stay_left_directions[i][Reward_NoReward_stay_left_i_columns]==2).sum(axis=1)
+        Reward_NoReward_stay_left_i_l = (Reward_NoReward_stay_left_directions[i][Reward_NoReward_stay_left_i_columns]==1).sum(axis=1)
+                
+        
+        
+        Reward_NoReward_switch_left_i_columns = list(Reward_NoReward_switch_left_directions[i].columns.values)
+        Reward_NoReward_switch_left_i_r = (Reward_NoReward_switch_left_directions[i][Reward_NoReward_switch_left_i_columns]==2).sum(axis=1)
+        Reward_NoReward_switch_left_i_l = (Reward_NoReward_switch_left_directions[i][Reward_NoReward_switch_left_i_columns]==1).sum(axis=1)
+          
+        
+        
+        
+    
+        if cellType==1:
+            
+            if sideOfStim =="R":
+                
+                
+                switchSide_switch_ipsi = Reward_NoReward_switch_left_i_r
+                switchSide_switch_contra = Reward_NoReward_switch_left_i_l
+                switchSide_switch_total = Reward_NoReward_switch_left_i_r+Reward_NoReward_switch_left_i_l
+                switchSide_switch_ratio = Reward_NoReward_switch_left_i_r/switchSide_switch_total
+                switchSide_Stay_ipsi = Reward_NoReward_stay_right_i_r
+                switchSide_Stay_contra = Reward_NoReward_stay_right_i_l
+                switchSide_Stay_total = Reward_NoReward_stay_right_i_r+Reward_NoReward_stay_right_i_l
+                switchSide_Stay_ratio = Reward_NoReward_stay_right_i_r/switchSide_Stay_total
+
+                
+                NOswitchSide_switch_ipsi = Reward_NoReward_switch_right_i_r
+                NOswitchSide_switch_contra = Reward_NoReward_switch_right_i_l
+                NOswitchSide_switch_total =Reward_NoReward_switch_right_i_r+Reward_NoReward_switch_right_i_l
+                NOswitchSide_switch_ratio =Reward_NoReward_switch_right_i_r/NOswitchSide_switch_total
+                NOswitchSide_Stay_ipsi = Reward_NoReward_stay_left_i_r
+                NOswitchSide_Stay_contra = Reward_NoReward_stay_left_i_l
+                NOswitchSide_Stay_total = Reward_NoReward_stay_left_i_r+Reward_NoReward_stay_left_i_l
+                NOswitchSide_Stay_ratio = Reward_NoReward_stay_left_i_r/NOswitchSide_Stay_total
+                
+                
+      
+            elif sideOfStim =="L":
+                
+                
+                switchSide_switch_contra = Reward_NoReward_switch_right_i_r
+                switchSide_switch_ipsi = Reward_NoReward_switch_right_i_l
+                switchSide_switch_total = Reward_NoReward_switch_right_i_l+Reward_NoReward_switch_right_i_r
+                switchSide_switch_ratio = Reward_NoReward_switch_right_i_l/switchSide_switch_total
+                switchSide_Stay_contra = Reward_NoReward_stay_left_i_r
+                switchSide_Stay_ipsi = Reward_NoReward_stay_left_i_l
+                switchSide_Stay_total = Reward_NoReward_stay_left_i_l+Reward_NoReward_stay_left_i_r
+                switchSide_Stay_ratio = Reward_NoReward_stay_left_i_l/switchSide_Stay_total
+                
+                NOswitchSide_switch_contra = Reward_NoReward_switch_left_i_r
+                NOswitchSide_switch_ipsi = Reward_NoReward_switch_left_i_l
+                NOswitchSide_switch_total = Reward_NoReward_switch_left_i_l+Reward_NoReward_switch_left_i_r
+                NOswitchSide_switch_ratio = Reward_NoReward_switch_left_i_l/NOswitchSide_switch_total
+                NOswitchSide_Stay_contra = Reward_NoReward_stay_right_i_r
+                NOswitchSide_Stay_ipsi = Reward_NoReward_stay_right_i_l
+                NOswitchSide_Stay_total = Reward_NoReward_stay_right_i_l+Reward_NoReward_stay_right_i_r
+                NOswitchSide_Stay_ratio = Reward_NoReward_stay_right_i_l/NOswitchSide_Stay_total
+                
+            
+        if cellType==2:
+            
+            if sideOfStim =="R":
+                
+                
+                switchSide_switch_ipsi = Reward_NoReward_switch_right_i_r
+                switchSide_switch_contra = Reward_NoReward_switch_right_i_l
+                switchSide_switch_total = Reward_NoReward_switch_right_i_l+Reward_NoReward_switch_right_i_r
+                switchSide_switch_ratio = Reward_NoReward_switch_right_i_l/switchSide_switch_total
+                switchSide_Stay_ipsi = Reward_NoReward_stay_left_i_r
+                switchSide_Stay_contra = Reward_NoReward_stay_left_i_l
+                switchSide_Stay_total = Reward_NoReward_stay_left_i_l+Reward_NoReward_stay_left_i_r
+                switchSide_Stay_ratio = Reward_NoReward_stay_left_i_l/switchSide_Stay_total
+                
+                NOswitchSide_switch_ipsi = Reward_NoReward_switch_left_i_r
+                NOswitchSide_switch_contra = Reward_NoReward_switch_left_i_l
+                NOswitchSide_switch_total = Reward_NoReward_switch_left_i_l+Reward_NoReward_switch_left_i_r
+                NOswitchSide_switch_ratio = Reward_NoReward_switch_left_i_l/NOswitchSide_switch_total
+                NOswitchSide_Stay_ipsi = Reward_NoReward_stay_right_i_r
+                NOswitchSide_Stay_contra = Reward_NoReward_stay_right_i_l
+                NOswitchSide_Stay_total = Reward_NoReward_stay_right_i_l+Reward_NoReward_stay_right_i_r
+                NOswitchSide_Stay_ratio = Reward_NoReward_stay_right_i_l/NOswitchSide_Stay_total
+                
+            elif sideOfStim =="L":
+            
+                switchSide_switch_contra = Reward_NoReward_switch_right_i_r
+                switchSide_switch_ipsi = Reward_NoReward_switch_right_i_l
+                switchSide_switch_total = Reward_NoReward_switch_right_i_r+Reward_NoReward_switch_right_i_l
+                switchSide_switch_ratio = Reward_NoReward_switch_right_i_r/switchSide_switch_total
+                switchSide_Stay_contra = Reward_NoReward_stay_left_i_r
+                switchSide_Stay_ipsi = Reward_NoReward_stay_left_i_l
+                switchSide_Stay_total=Reward_NoReward_stay_left_i_r+Reward_NoReward_stay_left_i_l
+                switchSide_Stay_ratio=Reward_NoReward_stay_left_i_r/switchSide_Stay_total
+                
+                NOswitchSide_switch_contra = Reward_NoReward_switch_right_i_r
+                NOswitchSide_switch_ipsi = Reward_NoReward_switch_right_i_l
+                NOswitchSide_switch_total = Reward_NoReward_switch_right_i_r+Reward_NoReward_switch_right_i_l
+                NOswitchSide_switch_ratio = Reward_NoReward_switch_right_i_r/NOswitchSide_switch_total
+                NOswitchSide_Stay_contra = Reward_NoReward_stay_left_i_r
+                NOswitchSide_Stay_ipsi = Reward_NoReward_stay_left_i_l
+                NOswitchSide_Stay_total = Reward_NoReward_stay_left_i_r+Reward_NoReward_stay_left_i_l
+                NOswitchSide_Stay_ratio = Reward_NoReward_stay_left_i_r/NOswitchSide_Stay_total
+                
+        
+        dfResults = pd.DataFrame({'switchSide_switch_ipsi': switchSide_switch_ipsi,
+                                  'switchSide_switch_contra': switchSide_switch_contra,
+                                  'switchSide_switch_total':switchSide_switch_total,
+                                  'switchSide_switch_ratio':switchSide_switch_ratio,
+                                  "switchSide_Stay_ipsi": switchSide_Stay_ipsi,
+                                  "switchSide_Stay_contra":switchSide_Stay_contra,
+                                  'switchSide_Stay_total':switchSide_Stay_total,
+                                  "switchSide_Stay_ratio":switchSide_Stay_ratio,
+                                  "NOswitchSide_switch_ipsi": NOswitchSide_switch_ipsi,
+                                  "NOswitchSide_switch_contra": NOswitchSide_switch_contra,
+                                  "NOswitchSide_switch_total":NOswitchSide_switch_total,
+                                  "NOswitchSide_switch_ratio":NOswitchSide_switch_ratio,
+                                  "NOswitchSide_Stay_ipsi": NOswitchSide_Stay_ipsi,
+                                  "NOswitchSide_Stay_contra": NOswitchSide_Stay_contra,
+                                  "NOswitchSide_Stay_total":NOswitchSide_Stay_total,
+                                  "NOswitchSide_Stay_ratio":NOswitchSide_Stay_ratio})
+        
+        
+        data_list.append(dfResults)
+       
+    return (data_list)
+
+
+
+
+
+
+
+
+
+
+
+
+
